c485b1769814581ae79f5bb772ff7296d53c91d8
Isaac Gorman <emailme@isaacg.net>
2020-07-17 11:20:01 -0500

    data received

--- starwars/src/components/ChoicesForm.js
+++ starwars/src/components/ChoicesForm.js
@@ -3,7 +3,20 @@ import axios from "axios";
 
 function ChoicesForm() {
   const [choiceArray, setChoiceArray] = useState({});
+  const [selection, setSelection] = useState("");
+  const [cardData, setCardData] = useState([]);
 
+  const handleChange = (e) => {
+    setSelection(e.target.value);
+    console.log("selection value,", selection);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    requestData();
+  };
+
+  // Selection
   useEffect(() => {
     axios.get("https://rickandmortyapi.com/api/").then((res) => {
       console.log("i am res!", res.data);
@@ -11,20 +24,70 @@ function ChoicesForm() {
     });
   }, []);
 
+  // API Call After Aelection
+  // "https://rickandmortyapi.com/api/character",
+
+  function requestData() {
+    console.log(
+      "I am the value of selection in api call",
+      selection.slice(0, -1)
+    );
+    console.log(`https://rickandmortyapi.com/api/${selection.slice(0, -1)}`);
+    axios
+      .get(`https://rickandmortyapi.com/api/${selection.slice(0, -1)}`)
+      .then((res) => {
+        console.log("cardData!", res.data.results);
+        setCardData(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err, "found!");
+      });
+  }
+
+  // useEffect(() => {
+  //   console.log(
+  //     "I am the value of selection in api call",
+  //     selection.slice(0, -1)
+  //   );
+  //   console.log(`https://rickandmortyapi.com/api/${selection.slice(0, -1)}`);
+  //   axios
+  //     .get(`https://rickandmortyapi.com/api/${selection.slice(0, -1)}`)
+  //     .then((res) => {
+  //       console.log("cardData!", res.data);
+  //       setCardData(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err, "found!");
+  //     });
+  // }, [selection, setSelection]);
+
   return (
     // Object.keys(user)
     <div>
       <h1>Test!</h1>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="topic-selection">
           Select Topic:
-          <select name="topic-selection" id="topic-selection">
-            <option value="placeholder">--Select--</option>
+          <br />
+          <select
+            name="topic-selection"
+            id="topic-selection"
+            onChange={(e) => handleChange(e)}
+          >
+            <option value="placeholder" key="0">
+              --Select--
+            </option>
             {Object.keys(choiceArray).map((crrV) => {
-              return <option key={crrV}>{crrV}</option>;
+              return (
+                <option value={crrV} key={crrV}>
+                  {crrV}
+                </option>
+              );
             })}
           </select>
         </label>
+        <br />
+        <button>Submit!</button>
       </form>
     </div>
   );
